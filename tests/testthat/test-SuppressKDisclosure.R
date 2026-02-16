0c81c1d9b80099f443a130cf24079307fc6bbb9c
Langsrud <oyl@ssb.no>
2026-02-16 14:58:30 +0100

    Add tests based on advanced examples using `targeting_exclude` and `targeting_include`
    - Rename previous output objects to avoid conflicts

--- tests/testthat/test-SuppressKDisclosure.R
+++ tests/testthat/test-SuppressKDisclosure.R
@@ -78,10 +78,10 @@ test_that("SuppressKDisclosure", {
   targ[[1]][10:11, 1] <- "99"   # to test matching
   targ[[2]][10:11, 1] <- "999"  # 
   
-  a1 <- SuppressKDisclosure(d, dimVar = 1:4, freqVar = "freq", 
+  o1 <- SuppressKDisclosure(d, dimVar = 1:4, freqVar = "freq", 
                             coalition = 55, extend0 = FALSE, 
                             targeting = targ, whenEmptyUnsuppressed = NULL,
-                            printInc = printInc, print_frames = TRUE, output = "all")
+                            printInc = printInc, print_frames = print_frames, output = "all")
   
   me <- Matrix::Matrix(FALSE, nrow(targ$sensitive), nrow(targ$identifying))
   
@@ -92,13 +92,13 @@ test_that("SuppressKDisclosure", {
   targ$exclude_relations <- me
   
   
-  a2 <- SuppressKDisclosure(d, dimVar = 1:4, freqVar = "freq", coalition = 55, 
+  o2 <- SuppressKDisclosure(d, dimVar = 1:4, freqVar = "freq", coalition = 55, 
                             extend0 = FALSE, targeting = targ, 
                             whenEmptyUnsuppressed = NULL,
-                            printInc = printInc, print_frames = TRUE, 
+                            printInc = printInc, print_frames = print_frames, 
                             output = "all")
   
-  expect_identical(c(ncol(a2$xExtraPrimary), ncol(a1$xExtraPrimary)), 23:24)
+  expect_identical(c(ncol(o2$xExtraPrimary), ncol(o1$xExtraPrimary)), 23:24)
   
   
   targ$targeting_exclude <- list(
@@ -106,19 +106,19 @@ test_that("SuppressKDisclosure", {
          identifying = list(region = "1", main_income = "Total"))
   )
   
-  a3 <- SuppressKDisclosure(d, dimVar = 1:4, freqVar = "freq", coalition = 55, 
+  o3 <- SuppressKDisclosure(d, dimVar = 1:4, freqVar = "freq", coalition = 55, 
                             extend0 = FALSE,
                             sensitive = sensitive[[2]], 
                             identifying = list(region = "!8", main_income = "*"),
                             whenEmptyUnsuppressed = NULL,
-                            printInc = printInc, print_frames = TRUE, 
+                            printInc = printInc, print_frames = print_frames, 
                             output = "all")
   
-  identical(as.vector(table(SSBtools::DummyDuplicated(cbind(a1$xExtraPrimary, a3$xExtraPrimary), rnd = TRUE))),
+  identical(as.vector(table(SSBtools::DummyDuplicated(cbind(o1$xExtraPrimary, o3$xExtraPrimary), rnd = TRUE))),
             c(24L, 24L))
   
   
-  a4 <- SuppressKDisclosure(d, dimVar = 1:4, freqVar = "freq", coalition = 55, 
+  o4 <- SuppressKDisclosure(d, dimVar = 1:4, freqVar = "freq", coalition = 55, 
                             extend0 = FALSE,
                             sensitive = sensitive[[2]], 
                             identifying = list(region = "!8", main_income = "*"),
@@ -126,14 +126,14 @@ test_that("SuppressKDisclosure", {
                               list(sensitive = list(region = "A", main_income = "assistance"),
                                    identifying = list(region = "1", main_income = "Total"))),
                             whenEmptyUnsuppressed = NULL,
-                            printInc = printInc, print_frames = TRUE, 
+                            printInc = printInc, print_frames = print_frames, 
                             output = "all")
   
-  identical(as.vector(table(SSBtools::DummyDuplicated(cbind(a2$xExtraPrimary, a4$xExtraPrimary), rnd = TRUE))),
+  identical(as.vector(table(SSBtools::DummyDuplicated(cbind(o2$xExtraPrimary, o4$xExtraPrimary), rnd = TRUE))),
             c(23L, 22L))
   
   
-  a5 <- SuppressKDisclosure(d, dimVar = 1:4, freqVar = "freq", coalition = 55, 
+  o5 <- SuppressKDisclosure(d, dimVar = 1:4, freqVar = "freq", coalition = 55, 
                             extend0 = FALSE,
                             sensitive = sensitive[[2]], 
                             identifying = list(region = "!8", main_income = "*"),
@@ -141,10 +141,77 @@ test_that("SuppressKDisclosure", {
                               list(sensitive = data.frame(region = "A", main_income = "assistance"),
                                    identifying = data.frame(region = "1", main_income = "Total"))),
                             whenEmptyUnsuppressed = NULL,
-                            printInc = printInc, print_frames = TRUE, 
+                            printInc = printInc, print_frames = print_frames, 
                             output = "all")
   
-  identical(as.vector(table(SSBtools::DummyDuplicated(cbind(a2$xExtraPrimary, a5$xExtraPrimary), rnd = TRUE))),
+  identical(as.vector(table(SSBtools::DummyDuplicated(cbind(o2$xExtraPrimary, o5$xExtraPrimary), rnd = TRUE))),
             c(23L, 23L))
+  
+  
+  
+  # Create a wrapper function to avoid repeating common arguments                                
+  fun <- function(..., coalition = 7) {
+    SuppressKDisclosure(SSBtoolsData("d3"), 
+                        formula = ~(region + county)*main_income + region*months + county*main_income*months, 
+                        freqVar = "freq", coalition = coalition ,
+                        mc_hierarchies = list(main_income = c("special = assistance + other", 
+                                                              "ordinary = pensions + wages")),
+                        printInc = printInc, print_frames = print_frames, output = "all",
+                        ...)}
+  
+  
+  # As above, but additionally exclude regions i and j via the sensitive specification          
+  a5 <- fun(sensitive = list(main_income = c("assistance", "wages")), 
+            identifying = list(region = "*", main_income = c("special", "ordinary")), 
+            targeting_exclude = list(list(sensitive = list(region = c("i", "j")))))
+  
+  # Same exclusion as above, but specified via identifying instead of sensitive
+  # Here `main_income` must also be specified, since the default for identifying is "Total" 
+  a6 <- fun(sensitive = list(main_income = c("assistance", "wages")), 
+            identifying = list(region = "*", main_income = c("special", "ordinary")), 
+            targeting_exclude = list(list(identifying = list(region = c("i", "j"), 
+                                                             main_income = "*"))))
+  
+  
+  
+  # As above, but use a data.frame for precise specification of relations
+  # Therefore, "V ordinary–pensions" is no longer included                                     
+  a8 <- fun(sensitive = list(main_income = c("assistance", "wages")), 
+            identifying = list(region = "*", main_income = c("special", "ordinary")), 
+            targeting_exclude = list(list(identifying = list(region = c("i", "j"), 
+                                                             main_income = "*"))), 
+            targeting_include = list(
+              list(identifying = data.frame(region = c("14", "U", "V", "X"), 
+                                            main_income = c("special", "ordinary"), 
+                                            months = c("m10m12", "Total")), 
+                   sensitive = list(region = c("m01m05"), 
+                                    main_income = c("pensions", "assistance")))))    
+  
+  # Specify the same relations as above, but in a different way
+  # Using multiple list elements                                    
+  a9 <- fun(sensitive = list(main_income = c("assistance", "wages")), 
+            identifying = list(region = "*", main_income = c("special", "ordinary")), 
+            targeting_exclude = list(list(identifying = list(region = c("i", "j"), 
+                                                             main_income = "*"))), 
+            targeting_include = list(
+              list(identifying = list(region = "14", 
+                                      main_income = "special", 
+                                      months = "m10m12"), 
+                   sensitive = list(region = "14", 
+                                    main_income = "assistance", 
+                                    months = "m10m12")), 
+              list(identifying = list(region = c("U", "X"), 
+                                      main_income = "ordinary", 
+                                      months = "Total"), 
+                   sensitive = list(region = c("U", "X"), 
+                                    main_income = "pensions", 
+                                    months = "Total")))) 
+  
+  expect_identical(c(ncol(a5$xExtraPrimary), ncol(a6$xExtraPrimary), 
+                     ncol(a8$xExtraPrimary), ncol(a9$xExtraPrimary)),
+                   c(22L, 22L, 25L, 25L))
+  
+  expect_identical(a5$publish, a6$publish)
+  expect_identical(a8$publish, a9$publish)
   
 })
